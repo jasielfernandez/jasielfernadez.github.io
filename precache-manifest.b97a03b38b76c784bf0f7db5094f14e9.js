2748f0cee72f35b59761652a3eb9a0ba33769987
jasielfernandez <jasielf12@gmail.com>
2020-05-04 22:02:41 -0500

    Add files via upload

---
+++ precache-manifest.b97a03b38b76c784bf0f7db5094f14e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea87e5939b96281fe62d14f914066f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b62c0332d498da38006",
+    "url": "/static/css/main.8e230b11.chunk.css"
+  },
+  {
+    "revision": "6dcddac9306dfe4b1612",
+    "url": "/static/js/2.f94d280f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f94d280f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b62c0332d498da38006",
+    "url": "/static/js/main.6d20d255.chunk.js"
+  },
+  {
+    "revision": "34cc9e86b84fa9d9da1c",
+    "url": "/static/js/runtime-main.5ce9eb8a.js"
+  }
+]);
